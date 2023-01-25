85975491929684637a4c96ee89dd49cd3109b848
Jeremy Alvarez <jeremy932012@gmail.com>
2023-01-25 23:50:37 +0100

    fix: add conditional loading if session are not available

--- hooks/useTableauQuery.tsx
+++ hooks/useTableauQuery.tsx
@@ -3,11 +3,13 @@ import { useAtom } from 'jotai'
 import { QueryFunction, QueryKey } from '@tanstack/query-core'
 import { useQuery, UseQueryOptions } from '@tanstack/react-query'
 import { LoadingAtom } from '../atoms/loadingAtom'
+import { useSession } from 'next-auth/react'
 
 export function useTableauQuery<TData>(
     queryKey: QueryKey,
     options?: UseQueryOptions
 ) {
+    const { data: session } = useSession()
     const [_isLoading, setLoading] = useAtom(LoadingAtom)
 
     const query = useQuery<TData>(
@@ -18,6 +20,7 @@ export function useTableauQuery<TData>(
     const { isFetching, isLoading, isRefetching } = query
 
     useEffect(() => {
+        if (!session) return
         if (isFetching || isLoading || isRefetching) return setLoading(true)
         setLoading(false)
     }, [isFetching, isLoading, isRefetching, setLoading])
