76c9111f3965f127a69976ec651d46b33f6cfa24
Jeremy Alvarez <jeremy932012@gmail.com>
2023-01-29 02:51:47 +0100

    feat: finish implementation of delete functionality, add transaction on db to be able to do cascade delete

---
+++ components/body/components/side/components/boardDelete.tsx
@@ -0,0 +1,67 @@
+import React, { useCallback } from 'react'
+import axios from 'axios'
+import { IconButton, Tooltip, useDisclosure } from '@chakra-ui/react'
+import { BsTrashFill } from 'react-icons/bs'
+
+import { DeleteModal } from '../../modal/deleteModal'
+import { useTableauMutation } from '../../../../../hooks/useTableauMutation'
+import { IBoardWithAllRelation } from '../../../../../types/types'
+import { useAtom } from 'jotai'
+import { BoardAtom } from '../../../../../atoms/boardAtom'
+import { RefetchBoardAtom } from '../../../../../atoms/refetchBoardAtom'
+
+interface IBoardDeleteProps {
+    isVisible: boolean
+    singleBoard: IBoardWithAllRelation
+}
+
+export function BoardDelete(props: IBoardDeleteProps) {
+    const { isVisible, singleBoard } = props
+
+    const [_selectedBoard, setSelectedBoard] = useAtom(BoardAtom)
+    const [refetchBoards] = useAtom(RefetchBoardAtom)
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const { mutateAsync } = useTableauMutation(
+        (board: IBoardWithAllRelation) => {
+            return axios.post(`api/board/delete`, board, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+            })
+        }
+    )
+
+    const onBoardDelete = useCallback(() => {
+        mutateAsync(singleBoard).then(() => {
+            onClose()
+            setSelectedBoard(null)
+            refetchBoards.fetch()
+        })
+    }, [mutateAsync, singleBoard, onClose, refetchBoards, setSelectedBoard])
+
+    return (
+        <>
+            <Tooltip label="Delete current board">
+                <IconButton
+                    onClick={() => onOpen()}
+                    colorScheme="teal"
+                    _hover={{ bgColor: 'red.500', color: 'white' }}
+                    variant={isVisible ? 'solid' : 'ghost'}
+                    aria-label="Delete Board"
+                    icon={<BsTrashFill />}
+                    size="sm"
+                    ml={2}
+                />
+            </Tooltip>
+            <DeleteModal
+                title="Delete Board"
+                isOpen={isOpen}
+                onClose={onClose}
+                onSubmit={onBoardDelete}
+            />
+        </>
+    )
+}
