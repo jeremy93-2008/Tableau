c3e6fe741ed76fcf5fd751c8649ed27e93617343
Jeremy Alvarez <jeremy932012@gmail.com>
2023-02-03 19:01:24 +0100

    build: add build configuration to execute monorepo on docker. test integration with shared-utils package

--- apps/Tableau/pages/_app.tsx
+++ apps/Tableau/pages/_app.tsx
@@ -6,7 +6,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import theme from '../theme/extends'
-import { defaultQueryFn } from '../utils/defaultQueryFn'
+import { defaultQueryFn } from 'shared-utils'
 
 // Create a client
 const queryClient = new QueryClient({
---
+++ packages/utils/src/calculateOverflow.ts
@@ -0,0 +1,19 @@
+export function calculateOverflow(
+    element: HTMLElement,
+    scrollParent: HTMLElement
+) {
+    const rectElement = element.getBoundingClientRect()
+    const rectScrollParentElement = scrollParent.getBoundingClientRect()
+
+    const bottomElementHeight = rectElement.top + rectElement.height
+    const bottomScrollParentElement =
+        rectScrollParentElement.top + rectScrollParentElement.height
+
+    const topElementHeight = rectElement.top + rectElement.height
+    const topScrollParentElement = rectScrollParentElement.top
+
+    return {
+        scrollTop: topElementHeight - topScrollParentElement,
+        scrollBottom: bottomElementHeight - bottomScrollParentElement,
+    }
+}
