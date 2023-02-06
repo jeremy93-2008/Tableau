d978ecc86d4b28bc19512516d31761b5ddb08f66
Jeremy Alvarez <jeremy932012@gmail.com>
2023-02-06 14:32:53 +0100

    chore(tableau-plus): No access to tableau plus available

--- apps/Tableau-plus/pages/index.tsx
+++ apps/Tableau-plus/pages/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Provider } from 'jotai'
-import { Flex } from '@chakra-ui/react'
-import { Header } from '../components/header'
-import { Body } from '../components/body'
+import {SiFoodpanda} from "react-icons/si"
+import { Flex, Link, Box, Spinner } from "@chakra-ui/react";
 import { LoadingProvider } from 'shared-components'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { DndProvider } from 'react-dnd'
@@ -12,10 +11,21 @@ export default function Home() {
         <Flex>
             <Provider>
                 <DndProvider backend={HTML5Backend}>
-                    <LoadingProvider>
-                        <Header />
-                        <Body />
-                    </LoadingProvider>
+                    <Flex width="100vw" height="100vh" flexDirection="column" justifyContent="center" alignItems="center" mx={4} my={4}>
+                      <Flex bgColor="teal.200" p={10} borderRadius={10} flexDirection="column" alignItems="center">
+                        <SiFoodpanda size={"250px"} />
+                        <Box mt={3} textAlign="center">
+                          No ready, still in development, go to the Tableau
+                          application:
+                        </Box>
+                        <Box>
+                          <Link color="teal.800" colorScheme="teal" href="https://tableau-jeremy93-2008.vercel.app/">
+                            https://tableau-jeremy93-2008.vercel.app/
+                          </Link>
+                        </Box>
+                      </Flex>
+                      </Flex>
+
                 </DndProvider>
             </Provider>
         </Flex>
