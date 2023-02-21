d3b7d52ec16eb1e7eb9207ac86726a772382e740
Jeremy Alvarez <jeremy932012@gmail.com>
2023-02-21 21:18:52 +0100

    fix(title): add new title and fix a dom attribute

--- web/tableau-web/src/components/assets/gridIcon.tsx
+++ web/tableau-web/src/components/assets/gridIcon.tsx
@@ -10,7 +10,7 @@ export function GridIcon(props: React.SVGProps<SVGSVGElement>) {
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
-            <path d="M0 0H58.51V58.51H0V0Z" fill="white" fill-opacity="0.01" />
+            <path d="M0 0H58.51V58.51H0V0Z" fill="white" fillOpacity="0.01" />
             <path
                 d="M49.9773 4.87582H8.53273C6.5131 4.87582 4.87585 6.51307 4.87585 8.5327V49.9773C4.87585 51.997 6.5131 53.6342 8.53273 53.6342H49.9773C51.997 53.6342 53.6342 51.997 53.6342 49.9773V8.5327C53.6342 6.51307 51.997 4.87582 49.9773 4.87582Z"
                 stroke="#319795"
