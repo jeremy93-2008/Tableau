be0ff4a09df329930887059d90ae060d741fd625
Jeremy Alvarez <jeremy932012@gmail.com>
2023-02-28 18:37:04 +0100

    chore: delete the tableau plus project

--- apps/Tableau-plus/server/prisma/getBoardIdFromStatusId.ts
+++
@@ -1,7 +0,0 @@
-import prisma from '../../lib/prisma'
-
-export async function getBoardIdFromStatusId(id: string) {
-    return (await prisma.statusBoard.findFirst({
-        where: { id },
-    }))!.boardId
-}
--- apps/Tableau-plus/server/utils/cleanOrder.ts
+++
@@ -1,8 +0,0 @@
-export type IReorderRows<TData> = TData & { id: string; order: number }
-
-export function cleanOrder<TData>(rows: IReorderRows<TData>[]) {
-    const orderedRows = rows.sort((a, b) => a.order - b.order)
-    return orderedRows.map((row, idx) => {
-        return { ...row, order: idx }
-    })
-}
