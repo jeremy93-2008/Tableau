68586d4657bd508afaf33db633366405823f3fa2
Jeremy Alvarez <jeremy932012@gmail.com>
2023-09-04 23:16:57 +0200

    feat: development refresh button change on UI, now in top right of comments section.

--- packages/components/src/components/columns/components/taskEditForm/components/taskEditFormButtonRefresh.tsx
+++ packages/components/src/components/columns/components/taskEditForm/components/taskEditFormButtonRefresh.tsx
@@ -1,7 +1,6 @@
 import { Flex, Button, Spinner } from '@chakra-ui/react'
 import { IoIosRefresh } from 'react-icons/io'
 import React, { useCallback, useState } from 'react'
-import { useTaskFormButtonRefreshVisibility } from '../hooks/useTaskFormButtonRefreshVisibility'
 
 interface ITaskEditFormButtonRefreshProps {
     onRefresh: () => Promise<void>
@@ -11,10 +10,10 @@ export function TaskEditFormButtonRefresh(
     props: ITaskEditFormButtonRefreshProps
 ) {
     const { onRefresh } = props
-    const [isLoading, setIsLoading] = useState(false)
 
-    const { isVisible, setIsVisible, stopInterval } =
-        useTaskFormButtonRefreshVisibility()
+    const [isVisible, setIsVisible] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isHovering, setIsHovering] = useState(false)
 
     const onRefreshClick = useCallback(() => {
         setIsLoading(true)
@@ -22,26 +21,52 @@ export function TaskEditFormButtonRefresh(
         onRefresh().then(() => {
             setIsLoading(false)
         })
-        stopInterval()
-    }, [stopInterval, onRefresh])
+    }, [setIsVisible, onRefresh])
+
+    const handleHoverRefresh = useCallback(
+        (isHover: boolean) => {
+            return () => {
+                setIsHovering(isHover)
+            }
+        },
+        [setIsHovering]
+    )
 
     return (
-        <Flex position="absolute" top={1} width="100%" justifyContent="center">
+        <Flex
+            position="absolute"
+            top={1}
+            right={5}
+            width="100%"
+            justifyContent="right"
+        >
             <Button
                 onClick={onRefreshClick}
+                onMouseEnter={handleHoverRefresh(true)}
+                onMouseLeave={handleHoverRefresh(false)}
                 disabled={isLoading}
                 aria-label="Refresh messages"
-                leftIcon={isLoading ? <Spinner size="xs" /> : <IoIosRefresh />}
                 size="sm"
                 zIndex={200}
+                width={isHovering ? 'auto' : 0}
+                maxW={isHovering ? '500px' : 0}
                 opacity={isVisible ? 1 : 0}
-                transition="opacity 0.2s"
-                bgColor="gray.600"
+                overflowX="hidden"
+                transition="opacity 0.2s, max-width 0.4s"
+                bgColor="gray.900"
                 _hover={{
-                    bgColor: 'gray.700',
+                    bgColor: 'gray.900',
                 }}
             >
-                {isLoading ? 'Fetching new messages' : 'Refresh messages'}
+                <Flex mr={isHovering ? 1 : 0}>
+                    {isLoading ? <Spinner size="xs" /> : <IoIosRefresh />}
+                </Flex>
+                <Flex>
+                    {isHovering &&
+                        (isLoading
+                            ? 'Fetching new messages'
+                            : 'Refresh messages')}
+                </Flex>
             </Button>
         </Flex>
     )
