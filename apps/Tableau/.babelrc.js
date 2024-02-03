4a79904c83959e1a788d62ceaf895eb1064514b1
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-02-03 12:08:20 +0100

    build: sole issues with babel plugins, switch to @next/swc

--- apps/Tableau/.babelrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: ['next/babel'],
-    plugins: [
-        process.env.NODE_ENV === 'development' ? 'istanbul' : '',
-        'babel-plugin-transform-typescript-metadata',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        'babel-plugin-parameter-decorator',
-    ],
-}
